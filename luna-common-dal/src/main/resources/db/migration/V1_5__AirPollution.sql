dd850f1452b7da74ab2e0ec84827022e0964d8f8
Li Site <8lisite@tongji.edu.cn>
2016-05-26 10:48:07 +0800

    AirPollution增删改查实现~~~

---
+++ luna-common-dal/src/main/resources/db/migration/V1_5__AirPollution.sql
@@ -0,0 +1,48 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50711
+ Source Host           : localhost
+ Source Database       : puck
+
+ Target Server Type    : MySQL
+ Target Server Version : 50711
+ File Encoding         : utf-8
+
+ Date: 05/26/2016 10:35:07 AM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `air_pollution_data`
+-- ----------------------------
+DROP TABLE IF EXISTS `air_pollution_data`;
+CREATE TABLE `air_pollution_data` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `day` int(11) NOT NULL,
+  `discharge` double DEFAULT NULL,
+  `month` int(11) NOT NULL,
+  `no` double DEFAULT NULL,
+  `smoke` double DEFAULT NULL,
+  `so2` double DEFAULT NULL,
+  `year` int(11) NOT NULL,
+  `air_pollution_source` bigint(20) DEFAULT NULL,
+  `airPollutionSource_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `air_pollution_data`
+-- ----------------------------
+BEGIN;
+INSERT INTO `air_pollution_data` VALUES ('2', '2014-12-19 10:29:44', null, null, '2', '0', '5', '0', '0', '0', '5', null, null), ('3', null, null, null, '2', null, '5', null, null, null, '5', null, null);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- luna-web/src/main/java/tiger/web/api/controller/air/AirQualityController.java
+++ luna-web/src/main/java/tiger/web/api/controller/air/AirQualityController.java
@@ -4,4 +4,7 @@ package tiger.web.api.controller.air;
  * Created by lisite on 16/5/21.
  */
 public class AirQualityController {
+
+
+
 }
