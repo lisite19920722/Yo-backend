444667a0c29c34aae3fa4ab5844447cf22b2a67b
hugo-xie <m15955038579@163.com>
2016-06-15 20:46:01 +0800

    keqiangindex

---
+++ luna-web/src/main/java/tiger/web/api/controller/EconomyKeQiang/KeQiangIndex.java
@@ -0,0 +1,18 @@
+package tiger.web.api.controller.EconomyKeQiang;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tiger.core.basic.BaseResult;
+import tiger.web.api.constants.APIConstants;
+
+///**
+// * Created by xy86 on 16/6/15.
+// */
+//@RestController
+//@RequestMapping(APIConstants.BASE_API_URL + "/economy")
+//public class KeQiangIndex {
+//
+//    @Autowired
+//    public BaseResult
+//}
